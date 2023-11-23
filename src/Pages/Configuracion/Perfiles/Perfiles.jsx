38890e06221da2ed8f769404b4b6b4f4b3d210ce
Isabella <collantemendezisabella@gmail.com>
2023-11-23 09:38:03 -0500

    feat(creacion carpeta proveedores)

--- src/Pages/Configuracion/Perfiles/Perfiles.jsx
+++ src/Pages/Configuracion/Perfiles/Perfiles.jsx
@@ -129,59 +129,58 @@ const Perfiles = () => {
           (permiso) =>
             permiso.permiso.toLowerCase() === Permisos_DB.CREAR_EDITAR
         ).length > 0 && (
-          <PButton
-            tooltip="Editar"
-            tooltipOptions={{ position: "top" }}
-            className="p-button-rounded p-mr-2"
-            onClick={(e) => editar_perfil(e, rowData.id_perfil)}
-          >
-            {Edit_Icono}
-          </PButton>
-        )}
+            <PButton
+              tooltip="Editar"
+              tooltipOptions={{ position: "top" }}
+              className="p-button-rounded p-mr-2"
+              onClick={(e) => editar_perfil(e, rowData.id_perfil)}
+            >
+              {Edit_Icono}
+            </PButton>
+          )}
         {permisosPerfil.filter(
           (permiso) => permiso.permiso.toLowerCase() === Permisos_DB.BORRAR
         ).length > 0 && (
-          <PButton
-            tooltip="Eliminar"
-            className="p-button-rounded p-button-danger p-mr-2"
-            tooltipOptions={{ position: "top" }}
-            onClick={(e) => modal_eliminar_perfil(e, rowData)}
-          >
-            {Trash_Icono}
-          </PButton>
-        )}
+            <PButton
+              tooltip="Eliminar"
+              className="p-button-rounded p-button-danger p-mr-2"
+              tooltipOptions={{ position: "top" }}
+              onClick={(e) => modal_eliminar_perfil(e, rowData)}
+            >
+              {Trash_Icono}
+            </PButton>
+          )}
       </div>
     );
   };
 
   const main = () => (
-    <>
-      <div className="w-5/6">
-        <Toast ref={toast} />
-        {modalVisible && (
-          <ModalAgregarPerfil
-            visible={modalVisible}
-            onClose={cambiar_visibilidad_modal}
-          />
-        )}
-        {verEliminarRestaurar && (
-          <EliminarRestaurar
-            tipo={"ELIMINAR"}
-            funcion={(e) =>
-              eliminar_restablecer_perfil(perfilState.id_perfil, e)
-            }
-          />
-        )}
+    <div className="w-5/6">
+      <Toast ref={toast} />
+      {modalVisible && (
+        <ModalAgregarPerfil
+          visible={modalVisible}
+          onClose={cambiar_visibilidad_modal}
+        />
+      )}
+      {verEliminarRestaurar && (
+        <EliminarRestaurar
+          tipo={"ELIMINAR"}
+          funcion={(e) =>
+            eliminar_restablecer_perfil(perfilState.id_perfil, e)
+          }
+        />
+      )}
 
-        <div className="flex justify-center gap-x-4 m-2 p-3">
-          <h1 className="text-3xl">Perfiles</h1>
-          <i className="pi pi-user" style={{ fontSize: "2rem" }}></i>
-        </div>
-        <div className="bg-white border my-3 p-3 rounded-sm w-full flex flex-wrap gap-3">
-          {permisosPerfil.filter(
-            (permiso) =>
-              permiso.permiso.toLowerCase() === Permisos_DB.CREAR_EDITAR
-          ).length > 0 && (
+      <div className="flex justify-center gap-x-4 m-2 p-3">
+        <h1 className="text-3xl">Perfiles</h1>
+        <i className="pi pi-user" style={{ fontSize: "2rem" }}></i>
+      </div>
+      <div className="bg-white border my-3 p-3 rounded-sm w-full flex flex-wrap gap-3">
+        {permisosPerfil.filter(
+          (permiso) =>
+            permiso.permiso.toLowerCase() === Permisos_DB.CREAR_EDITAR
+        ).length > 0 && (
             <Button
               tipo={"PRINCIPAL"}
               funcion={(e) => setModalVisible(true, e)}
@@ -189,53 +188,52 @@ const Perfiles = () => {
               {Add_Icono} Agregar{" "}
             </Button>
           )}
-          {permisosPerfil.filter(
-            (permiso) => permiso.permiso.toLowerCase() === Permisos_DB.CONSULTAR
-          ).length > 0 && (
+        {permisosPerfil.filter(
+          (permiso) => permiso.permiso.toLowerCase() === Permisos_DB.CONSULTAR
+        ).length > 0 && (
             <div className="h-full flex justify-center items-center">
               <BLink tipo={"INACTIVOS"} url="/configuracion/perfiles/inactivos">
                 Inactivos
               </BLink>
             </div>
           )}
-          <span className="p-input-icon-left sm:ml-auto md:ml-auto lg:ml-auto xl:ml-auto border rounded-md">
-            <i className="pi pi-search" />
-            <InputText
-              className="h-10 pl-8 rounded-md"
-              placeholder="Buscar"
-              onChange={(e) => buscador(e)}
-              value={searchTerm}
-            />
-          </span>
-        </div>
-
-        <div className="card">
-          <DataTable
-            className="custom-datatable"
-            stripedRows
-            value={filteredData}
-            paginator={true}
-            rows={5}
-            header={header}
-            emptyMessage="No se han encontrado resultados"
-            rowsPerPageOptions={[5, 10, 25, 50]}
-            paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-            currentPageReportTemplate="{first} to {last} of {totalRecords}"
-            tableStyle={{ minWidth: "50rem" }}
-          >
-            {visibleColumns.map((col) => (
-              <Column key={col.field} field={col.field} header={col.header} />
-            ))}
-
-            <Column
-              key="actions"
-              style={{ width: "10%" }}
-              body={(rowData) => columna_acciones(rowData)}
-            />
-          </DataTable>
-        </div>
+        <span className="p-input-icon-left sm:ml-auto md:ml-auto lg:ml-auto xl:ml-auto border rounded-md">
+          <i className="pi pi-search" />
+          <InputText
+            className="h-10 pl-8 rounded-md"
+            placeholder="Buscar"
+            onChange={(e) => buscador(e)}
+            value={searchTerm}
+          />
+        </span>
       </div>
-    </>
+
+      <div className="card">
+        <DataTable
+          className="custom-datatable"
+          stripedRows
+          value={filteredData}
+          paginator={true}
+          rows={5}
+          header={header}
+          emptyMessage="No se han encontrado resultados"
+          rowsPerPageOptions={[5, 10, 25, 50]}
+          paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+          currentPageReportTemplate="{first} to {last} of {totalRecords}"
+          tableStyle={{ minWidth: "50rem" }}
+        >
+          {visibleColumns.map((col) => (
+            <Column key={col.field} field={col.field} header={col.header} />
+          ))}
+
+          <Column
+            key="actions"
+            style={{ width: "10%" }}
+            body={(rowData) => columna_acciones(rowData)}
+          />
+        </DataTable>
+      </div>
+    </div>
   );
 
   return (
@@ -243,8 +241,8 @@ const Perfiles = () => {
       {permisosPerfil.length === 0 ? (
         <Loader />
       ) : permisosPerfil.filter(
-          (permiso) => permiso.permiso.toLowerCase() === Permisos_DB.CONSULTAR
-        ).length > 0 ? (
+        (permiso) => permiso.permiso.toLowerCase() === Permisos_DB.CONSULTAR
+      ).length > 0 ? (
         main()
       ) : (
         <Forbidden />
