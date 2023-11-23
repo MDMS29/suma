8d55f8077c0a54da19b5a8bb0c3b639cdd0b8efa
RosmyPachon <rosmypachon@gmail.com>
2023-11-23 17:29:37 -0500

    proveedores

--- src/context/Compras/ProveedoresProvider.jsx
+++ src/context/Compras/ProveedoresProvider.jsx
@@ -5,66 +5,66 @@ import useAuth from "../../hooks/useAuth";
 const ProveedoresContext = createContext();
 
 const ProveedoresProvider = ({ children }) => {
+  const { setAlerta, authUsuario } = useAuth();
+  const [permisosProveedor, setPermisosProveedor] = useState([]);
+  const [dataProveedores, setDataProveedores] = useState([]);
+  const [proveedorState, setProveedorState] = useState({});
 
-    const { setAlerta, authUsuario } = useAuth()
-    const [permisosProveedor, setPermisosProveedor] = useState([])
-    const [dataProveedores, setDataProveedores] = useState([])
-    const [proveedorState, setProveedorState] = useState({})
+  useEffect(() => {
+    if (location.pathname.includes("proveedores")) {
+      const obtener_proveedores = async () => {
+        const token = localStorage.getItem("token");
 
-
-    useEffect(() => {
-        if (location.pathname.includes('proveedores')) {
-            const obtener_proveedores = async () => {
-                const token = localStorage.getItem("token");
-
-                const config = {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`,
-                    },
-                };
-                const estado = location.pathname.includes("inactivos") ? 2 : 1;
-                if (authUsuario.id_empresa) {
-                    try {
-                        const { data } = await conexion_cliente(`/compras/proveedores?estado=${estado}&empresa=${authUsuario.id_empresa}`, config);
-                        if (data.error) {
-                            setAlerta({
-                                error: true,
-                                show: true,
-                                message: data.message
-                            })
-                        }
-                        console.log(data);
-                        if (data.error == false) {
-                            setDataProveedores([]);
-                            return
-                        }
-                        setDataProveedores(data);
-                    } catch (error) {
-                        return setDataProveedores([]);
-                    }
-                }
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        };
+        const estado = location.pathname.includes("inactivos") ? 2 : 1;
+        if (authUsuario.id_empresa) {
+          try {
+            const { data } = await conexion_cliente(
+              `/compras/proveedores?estado=${estado}&empresa=${authUsuario.id_empresa}`,
+              config
+            );
+            if (data.error) {
+              setAlerta({
+                error: true,
+                show: true,
+                message: data.message,
+              });
             }
-            obtener_proveedores()
+            console.log(data);
+            if (data.error == false) {
+              setDataProveedores([]);
+              return;
+            }
+            setDataProveedores(data);
+          } catch (error) {
+            return setDataProveedores([]);
+          }
         }
-    }, [location.pathname])
+      };
+      obtener_proveedores();
+    }
+  }, [location.pathname]);
 
-    const obj = useMemo(() => ({
-        permisosProveedor,
-        setPermisosProveedor,
-        dataProveedores,
-        setDataProveedores,
-        proveedorState,
-        setProveedorState
-    }))
+  const obj = useMemo(() => ({
+    permisosProveedor,
+    setPermisosProveedor,
+    dataProveedores,
+    setDataProveedores,
+    proveedorState,
+    setProveedorState,
+  }));
 
-    return (
-        <ProveedoresContext.Provider
-            value={obj}>
-            {children}
-        </ProveedoresContext.Provider>
-    )
-}
+  return (
+    <ProveedoresContext.Provider value={obj}>
+      {children}
+    </ProveedoresContext.Provider>
+  );
+};
 
-export { ProveedoresProvider }
-export default ProveedoresContext
+export { ProveedoresProvider };
+export default ProveedoresContext;
